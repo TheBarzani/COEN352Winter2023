b5739be3d20549b69910c4108292531910d416a2
TheBarzani <ismaelmergasori@gmail.com>
2023-02-05 22:08:24 -0500

    feat: addition of tutorial3 material v1

--- src/tut3/LinkedList/ListVsArray.java
+++ src/tut3/LinkedList/ListVsArray.java
@@ -2,6 +2,15 @@ package tut3.LinkedList;
 
 public class ListVsArray {
     public static void main(String[] args) {
-        //TODO: linked list vs arrays
+
+        SingleLL<Integer> myList = new SingleLL<>();
+        myList.insert(10);
+        myList.insert(2);
+        myList.insert(1);
+        myList.insert(9);
+        myList.insert(3);
+        //TODO:toString() method to be defined!
+        System.out.println(myList.toString());
+
     }
 }
